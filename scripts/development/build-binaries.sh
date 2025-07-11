38680ba0c5586e7c88f31dd37e788494b79dc71e
Matt Hogan <matt@nagoh.io>
2025-07-10 21:14:02 -0500

    Remove unused build platforms

--- scripts/development/build-binaries.sh
+++ scripts/development/build-binaries.sh
@@ -54,21 +54,21 @@ else
     echo "Building for all platforms..."
     # linux builds
     build_platform "linux-amd64" "linux" "amd64" ""
-    build_platform "linux-386" "linux" "386" ""
+    # build_platform "linux-386" "linux" "386" ""
     build_platform "linux-arm64" "linux" "arm64" ""
-    build_platform "linux-armv7" "linux" "arm" "7"
+    # build_platform "linux-armv7" "linux" "arm" "7"
 
     # macOS builds
-    build_platform "macos-x64" "darwin" "amd64" ""
-    build_platform "macos-arm64" "darwin" "arm64" ""
+    # build_platform "macos-x64" "darwin" "amd64" ""
+    # build_platform "macos-arm64" "darwin" "arm64" ""
 
     # Windows builds
-    build_platform "windows-x64" "windows" "amd64" ""
-    build_platform "windows-arm64" "windows" "arm64" ""
+    # build_platform "windows-x64" "windows" "amd64" ""
+    # build_platform "windows-arm64" "windows" "arm64" ""
 
     # FreeBSD builds
-    build_platform "freebsd-amd64" "freebsd" "amd64" ""
-    build_platform "freebsd-arm64" "freebsd" "arm64" ""
+    # build_platform "freebsd-amd64" "freebsd" "amd64" ""
+    # build_platform "freebsd-arm64" "freebsd" "arm64" ""
 fi
 
 echo "Compilation done"
